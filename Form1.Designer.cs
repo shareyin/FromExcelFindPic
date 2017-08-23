4b04faacde16a0b278cd89dccc1f48f0cfdc9a37
shareyin <shareyin@qq.com>
2017-08-23 13:55:52 +0800

    通过Excel找出图片
    通过Excel找出图片

---
+++ Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace FormExcelFindPic
+{
+    partial class labelCat
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBoxfile = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.labelCarNum = new System.Windows.Forms.Label();
+            this.labelOBUNum = new System.Windows.Forms.Label();
+            this.labelCarType = new System.Windows.Forms.Label();
+            this.labelOBUType = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 51);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(406, 360);
+            this.dataGridView1.TabIndex = 5;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 20);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(93, 24);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "导入Excel";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBoxfile
+            // 
+            this.textBoxfile.Location = new System.Drawing.Point(518, 12);
+            this.textBoxfile.Name = "textBoxfile";
+            this.textBoxfile.Size = new System.Drawing.Size(484, 21);
+            this.textBoxfile.TabIndex = 16;
+            this.textBoxfile.Text = "C:\\Users\\JG\\Desktop\\新版软件\\Debug";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(424, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 23);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "本地图片路径";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(435, 51);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(510, 360);
+            this.pictureBox1.TabIndex = 14;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.DoubleClick += new System.EventHandler(this.pictureBox1_DoubleClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(965, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 16);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "识别车牌:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(965, 139);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 16);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "OBU车牌:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(965, 228);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 16);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "识别车型:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.Location = new System.Drawing.Point(965, 316);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 16);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "OBU车型:";
+            // 
+            // labelCarNum
+            // 
+            this.labelCarNum.AutoSize = true;
+            this.labelCarNum.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labelCarNum.Location = new System.Drawing.Point(965, 84);
+            this.labelCarNum.Name = "labelCarNum";
+            this.labelCarNum.Size = new System.Drawing.Size(88, 16);
+            this.labelCarNum.TabIndex = 21;
+            this.labelCarNum.Text = "__________";
+            // 
+            // labelOBUNum
+            // 
+            this.labelOBUNum.AutoSize = true;
+            this.labelOBUNum.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labelOBUNum.Location = new System.Drawing.Point(965, 171);
+            this.labelOBUNum.Name = "labelOBUNum";
+            this.labelOBUNum.Size = new System.Drawing.Size(88, 16);
+            this.labelOBUNum.TabIndex = 22;
+            this.labelOBUNum.Text = "__________";
+            // 
+            // labelCarType
+            // 
+            this.labelCarType.AutoSize = true;
+            this.labelCarType.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labelCarType.Location = new System.Drawing.Point(965, 262);
+            this.labelCarType.Name = "labelCarType";
+            this.labelCarType.Size = new System.Drawing.Size(88, 16);
+            this.labelCarType.TabIndex = 23;
+            this.labelCarType.Text = "__________";
+            // 
+            // labelOBUType
+            // 
+            this.labelOBUType.AutoSize = true;
+            this.labelOBUType.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labelOBUType.Location = new System.Drawing.Point(965, 347);
+            this.labelOBUType.Name = "labelOBUType";
+            this.labelOBUType.Size = new System.Drawing.Size(88, 16);
+            this.labelOBUType.TabIndex = 24;
+            this.labelOBUType.Text = "__________";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(212, 12);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(71, 16);
+            this.radioButton1.TabIndex = 25;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "本地使用";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(323, 12);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(83, 16);
+            this.radioButton2.TabIndex = 26;
+            this.radioButton2.Text = "工控机使用";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // labelCat
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1090, 439);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.labelOBUType);
+            this.Controls.Add(this.labelCarType);
+            this.Controls.Add(this.labelOBUNum);
+            this.Controls.Add(this.labelCarNum);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxfile);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button2);
+            this.Name = "labelCat";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBoxfile;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label labelCarNum;
+        private System.Windows.Forms.Label labelOBUNum;
+        private System.Windows.Forms.Label labelCarType;
+        private System.Windows.Forms.Label labelOBUType;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using System.Text.RegularExpressions;
+using System.Diagnostics;
+
+namespace FormExcelFindPic
+{
+    public partial class labelCat : Form
+    {
+        public labelCat()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog filedialog = new OpenFileDialog();
+            string FileName = "";
+
+            if (filedialog.ShowDialog() == DialogResult.OK)
+            {
+                FileName = filedialog.FileName;
+                //((DataGridViewTextBoxColumn)dataGridView1.Columns[5]).MaxInputLength=1024;
+                dataGridView1.DataSource = GetExcelData(FileName);
+                dataGridView1.DataMember = "[Sheet1$]";
+
+                for (int count = 0; (count <= (dataGridView1.Rows.Count - 1)); count++)
+                {
+                    dataGridView1.Rows[count].HeaderCell.Value = (count + 1).ToString();
+                }
+            }
+        }
+        //获取Excel
+        public static DataSet GetExcelData(string str)
+        {
+            string strCon = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + str + ";Extended Properties='Excel 12.0;HDR=YES;IMEX=1;'";
+            OleDbConnection myConn = new OleDbConnection(strCon);
+            string strCom = " SELECT * FROM [Sheet1$]";
+            myConn.Open();
+            OleDbDataAdapter myCommand = new OleDbDataAdapter(strCom, myConn);
+            DataSet myDataSet = new DataSet();
+            myCommand.Fill(myDataSet, "[Sheet1$]");
+            myConn.Close();
+            return myDataSet;
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                int index = this.dataGridView1.CurrentRow.Index;
+                string s_mJGCarType = (this.dataGridView1.Rows[index].Cells[2].Value).ToString();//序号
+                string s_mCamPlateNum = (this.dataGridView1.Rows[index].Cells[5].Value).ToString();//车型
+                string s_mOBUCarType = (this.dataGridView1.Rows[index].Cells[10].Value).ToString();//车牌颜色
+                string s_mOBUPlateNum = (this.dataGridView1.Rows[index].Cells[9].Value).ToString();//车牌号码
+                string s_mImagePath = (this.dataGridView1.Rows[index].Cells[7].Value).ToString();//图片路径
+
+                Showpic(s_mJGCarType, s_mCamPlateNum, s_mOBUCarType, s_mOBUPlateNum, s_mImagePath);
+                //OpenIe(s_mImagePath);
+                //Process.Start(s_mImagePath);
+                //GridDouclickShowPicDialog gdspd = new GridDouclickShowPicDialog(vehtype, getplateno, imagepath, forcetime, ovehtype, oplatenumber, laser_vehlenth, laser_vehheight, this);
+
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                Console.WriteLine(ex.ToString());
+            }
+        }
+        //显示图片
+        public void Showpic(string s_mJGCarType, string s_mCamPlateNum, string s_mOBUCarType, string s_mOBUPlateNum, string s_mImagePath)
+        {
+            string fileName = System.IO.Path.GetFileName(s_mImagePath);
+            if (textBoxfile.Text != "")
+            {
+                if (radioButton1.Checked)
+                {
+                    s_mImagePath = textBoxfile.Text +"\\"+ fileName;
+                }
+                else
+                {
+                    s_mImagePath = textBoxfile.Text + s_mImagePath; 
+                }
+                
+            }
+            if (s_mImagePath != null || s_mImagePath != "" || s_mImagePath == "未知")
+            {
+                try
+                {
+                    pictureBox1.Load(s_mImagePath);
+                    pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("该记录无对应图片");
+                }
+            }
+            else
+            {
+                MessageBox.Show("该记录无对应图片");
+            }
+            labelCarNum.Text = s_mCamPlateNum;
+            labelCarType.Text = s_mJGCarType;
+            labelOBUNum.Text = s_mOBUPlateNum;
+            labelOBUType.Text = s_mOBUCarType;
+            //MessageBox.Show(s_mImagePath);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string path = string.Empty;
+            System.Windows.Forms.FolderBrowserDialog fbd = new System.Windows.Forms.FolderBrowserDialog();
+            if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                path = fbd.SelectedPath;
+            }
+            textBoxfile.Text = path;
+            //return path;
+        }
+
+        private void pictureBox1_DoubleClick(object sender, EventArgs e)
+        {
+            string PicPath = pictureBox1.ImageLocation;
+            Process.Start(PicPath);
+        }
+    }
+}
